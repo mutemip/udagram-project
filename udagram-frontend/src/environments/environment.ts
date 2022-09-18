e8183d3bf6fc14a5767007640e6f4fbe85f581ed
mutemip <paulmutemi2030@gmail.com>
2022-09-18 22:01:09 +0300

    port 8080 readded

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://aa99dc793acd44b768ac10918626ad04-1233401442.us-east-1.elb.amazonaws.com/api/v0'
+  apiHost: 'http://aa99dc793acd44b768ac10918626ad04-1233401442.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
